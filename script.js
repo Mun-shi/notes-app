9f77dfdae45e82a06e471d6e065d12d4aeefee4c
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2022-08-20 15:12:43 +0530

    update

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
-//import { marked } from 'marked';
+const addBtn = document.querySelector('.add');
 
-const notesEl = document.querySelector('.notes');
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
-
-const main = notesEl.querySelector('.main');
-const textArea = notesEl.querySelector('textarea');
-
-editBtn.addEventListener("click", () => {
-  main.classList.toggle("hidden");
-  textArea.classList.toggle("hidden");
+addBtn.addEventListener('click', () => {
+     addNewNote();
 });
 
-textArea.addEventListener("input", (e) => {
-  const { value } = e.target;
+function addNewNote() {
+    const note = document.createElement('div');
+    note.classList.add('note');
+    
+    note.innerHTML = `
+        <div class="notes">
+        <div class="tools">
+          <button class="edit"><i class="fas fa-edit"></i></button>
+          </button>
+          <button class="delete"><i class="fas fa-trash-alt"></i></button>
+          </button>   
+        </div>
+        <div class="main hidden">
+       
+        </div>
+        <textarea> 
+       </textarea>
+       </div>
+    `;
 
-  main.innerHTML = marked(value);
-} ); 
+    //const notesEl = note.querySelector('.notes');
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+    
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+    
+    editBtn.addEventListener("click", () => {
+      main.classList.toggle("hidden");
+      textArea.classList.toggle("hidden");
+    });
+
+    deleteBtn.addEventListener("click", () => {
+        note.remove(); 
+    });
+    
+    textArea.addEventListener("input", (e) => {
+      const { value } = e.target;
+    
+      main.innerHTML = marked(value);
+    } ); 
+    
+
+
+    document.body.appendChild(note);
+}
+
+
+
+/*  */
